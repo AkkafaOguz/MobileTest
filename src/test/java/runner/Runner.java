a54f49846064089897db4c3abe2d500cfaa48ed7
Oguzhan <akkafaoguz@gmail.com>
2021-12-08 21:15:33 +0300

    first day

---
+++ src/test/java/runner/Runner.java
@@ -0,0 +1,26 @@
+package runner;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+
+        plugin = { "pretty",
+                   "html:target/default-cucumber-reports"},
+        features = "src/test/resources/features",
+        glue = "stepDefinitions",
+        tags = "@mint",
+        dryRun = false
+
+
+
+)
+
+
+public class Runner {
+
+}
